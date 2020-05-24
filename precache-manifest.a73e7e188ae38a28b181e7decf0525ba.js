d2c83cf17a029b3dd370ad6d291b24ea0862e72f
Ming Zhao <michael.zhao@fluor.com>
2020-05-24 00:15:07 -0600

    Updates

---
+++ precache-manifest.a73e7e188ae38a28b181e7decf0525ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0330f1af5fd42f5930c0db3d6332cd26",
+    "url": "/mui-pokedex/index.html"
+  },
+  {
+    "revision": "011b9382aaabb860d86e",
+    "url": "/mui-pokedex/static/js/2.516f12a8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mui-pokedex/static/js/2.516f12a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61dae9a7863958a6ba32",
+    "url": "/mui-pokedex/static/js/main.24ff1076.chunk.js"
+  },
+  {
+    "revision": "111090861a86c5e95cef",
+    "url": "/mui-pokedex/static/js/runtime-main.3da61dde.js"
+  }
+]);
--- src/constants.js
+++
@@ -1,3 +0,0 @@
-export const toFirstCharUppercase = name => 
-    name.charAt(0).toUpperCase() + name.slice(1);
-export default toFirstCharUppercase
